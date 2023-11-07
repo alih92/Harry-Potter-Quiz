4a2d0373061df1fbaaa3287716ac857697c8dbec
Ali Halavurt <alihalavurt@Alis-MBP.broadband>
2023-11-07 12:55:12 +0000

    added all question into questions.js file as an array of objects

---
+++ starter/questions.js
@@ -0,0 +1,42 @@
+var questions = [
+  {
+    question: "What is Harry Potter's middle name?",
+    choices: ["Sirius", "Albus", "Remus", "James"],
+    correctAnswer: "James"
+  },
+  {
+    question: "What was Neville Longbottom's toad called?",
+    choices: ["Alan", "Max", "Longbottom", "Trevor"],
+    correctAnswer: "Trevor"
+  },
+  {
+    question: "What did Tom Riddle use to ensnare Ginny Weasley?",
+    choices: ["His good looks", "A potion", "A diary", "A magical spell"],
+    correctAnswer: "A diary"
+  },
+  {
+    question: "What animal was Peter Pettigrew able to transform into?",
+    choices: ["Bird", "Cat", "Dog", "Rat"],
+    correctAnswer: "Rat" 
+  },
+  {
+    question: "Which Death Eater was impersonating Mad-Eye Moody?",
+    choices: ["Igor Karkaroff", "Severus Snape", "Barty Crouch Jr.", "Lucius Malfoy"],
+    correctAnswer: "Barty Crouch Jr."
+  },
+  {
+    question: "Where does Sirius Black die?",
+    choices: ["Department of Mysteries", "Time Room", "Space Chmber", "Death Chamber"],
+    correctAnswer: "Death Chamber"
+  },
+  {
+    question: "Who is the Half-Blood Prince?",
+    choices: ["Harry Potter", "Draco Malfoy", "Tom Riddle", "Seerus Snape"],
+    correctAnswer: "Severus Snape"
+  },
+  {
+    questions: "What type of Patronus appears to lead Harry to the Sword of Gryffindor?",
+    choices: ["A stag", "A sea otter", "A doe", "A cat"],
+    correctAnswer: "A doe"
+  }
+];
