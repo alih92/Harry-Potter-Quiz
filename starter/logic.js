daa229de4367ba2b3cb450aeb3d312622135e573
Ali Halavurt <alihalavurt@Alis-MBP.broadband>
2023-11-10 17:17:49 +0000

    added more pseudocode for making questions/choices display and a timer

---
+++ starter/logic.js
@@ -0,0 +1,28 @@
+/*Harry Potter quiz pseudocode
+Which elements of the HTML do I need to target? Need to have these in variables to be able to reference and manipulate later on
+
+1. Quiz starts only when user clicks on the start button -> need an eventlistener for this with a function being called to display the questions
+  a. once the button is clicked, it, and any other text, disappears. How can i do this? hide? 
+2. Question title and choices need to show up after the start button is clicked and disappears. -> can this be a function to dislay the questions?
+  a. Iterate through the questions and get the text to show up -> use .textContent?
+    -> how can i keep a track of the questions index? 
+  b. Display the choices using .textContent ->choices is an array so maybe another for loop? use .textContent to display again. 
+3. set a timer -> set it to count down from , e.g. 100 - maybe use count--;
+  a. impelement a penalty of 10 seconds for each wrong answer - how can I do this? maybe some sort of decrementing option?
+  b. 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
